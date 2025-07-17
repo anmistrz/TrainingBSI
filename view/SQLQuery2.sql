b8c8af05cb1345e0f9d6868ac5906e642e2e4c24
Anas Ardiansyah <BSI80252@bsi.co.id>
2025-07-17 15:01:21 +0700

    fix: view exercise section 1

--- view/SQLQuery2.sql
+++ view/SQLQuery2.sql
@@ -2,7 +2,7 @@ CREATE VIEW vw_ActiveProductCatalog
 as
 	SELECT Name, ProductNumber, ListPrice, SellStartDate 
 	FROM Production.Product
-	WHERE SellEndDate IS NOT NULL
+	WHERE SellEndDate IS NULL
 go
 
 SELECT * FROM vw_ActiveProductCatalog;
